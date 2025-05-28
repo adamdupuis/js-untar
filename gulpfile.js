4876efd07b03b43ac15e1e5c5cdd18e50dd98cf8
Adam Dupuis <adam.dupuis@gmail.com>
2025-05-28 13:15:56 -0400

    Upgrade to gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,108 @@
 var gulp = require("gulp"),
-	umd = require("gulp-umd"),
-	sourcemaps = require("gulp-sourcemaps"),
-	uglify = require("gulp-uglify"),
-	insert = require("gulp-insert"),
-	addSrc = require("gulp-add-src"),
-	concat = require("gulp-concat"),
-	jshint = require("gulp-jshint"),
-	KarmaServer = require('karma').Server,
-	path = require("path"),
-	filter = require("gulp-filter"),
-    webserver = require('gulp-webserver');
+    umd = require("gulp-umd"),
+    sourcemaps = require("gulp-sourcemaps"),
+    uglify = require("gulp-uglify"),
+    insert = require("gulp-insert"),
+    concat = require("gulp-concat"),
+    jshint = require("gulp-jshint"),
+    KarmaServer = require('karma').Server,
+    path = require("path"),
+    filter = require("gulp-filter"),
+    webserver = require('gulp-webserver'),
+    merge = require('merge-stream');
 
 gulp.task("build:dev", function() {
-	var f = filter(['*', '!untar-worker.js'], { restore: true });
+    var f = filter(['*', '!untar-worker.js'], { restore: true });
 
-	return gulp.src(["src/untar.js"])
-		.pipe(sourcemaps.init())
-		.pipe(insert.append("\nworkerScriptUri = '/base/build/dev/untar-worker.js';"))
-		.pipe(addSrc(["src/ProgressivePromise.js", "src/untar-worker.js"]))
-		.pipe(jshint())
-		.pipe(jshint.reporter("default"))
-		.pipe(jshint.reporter("fail"))
-		.pipe(insert.prepend('"use strict";\n'))
-		.pipe(f)
-		.pipe(umd({
-			dependencies: function(file) {
-				if (path.basename(file.path) === "untar.js") {
-					return ["ProgressivePromise"];
-				} else {
-					return [];
-				}
-			},
-			exports: function(file) {
-				return path.basename(file.path, path.extname(file.path));
-			},
-			namespace: function(file) {
-				return path.basename(file.path, path.extname(file.path));
-			}
-		}))
-		.pipe(f.restore)
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest("build/dev"));
+    return gulp.src(["src/untar.js"])
+        .pipe(sourcemaps.init())
+        .pipe(insert.append("\nworkerScriptUri = '/base/build/dev/untar-worker.js';"))
+        .pipe(gulp.src(["src/ProgressivePromise.js", "src/untar-worker.js"])) // replace addSrc with gulp.src, merge streams if needed
+        .pipe(jshint({ esversion: 6 }))
+        .pipe(jshint.reporter("default"))
+        .pipe(jshint.reporter("fail"))
+        .pipe(insert.prepend('"use strict";\n'))
+        .pipe(f)
+        .pipe(umd({
+            dependencies: function(file) {
+                if (path.basename(file.path) === "untar.js") {
+                    return ["ProgressivePromise"];
+                } else {
+                    return [];
+                }
+            },
+            exports: function(file) {
+                return path.basename(file.path, path.extname(file.path));
+            },
+            namespace: function(file) {
+                return path.basename(file.path, path.extname(file.path));
+            }
+        }))
+        .pipe(f.restore)
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("build/dev"));
 });
 
 gulp.task("build:dist", function() {
-	return gulp.src("src/untar-worker.js")
+    // Process untar-worker.js first
+    var workerStream = gulp.src("src/untar-worker.js")
         .pipe(sourcemaps.init())
-		.pipe(jshint())
-		.pipe(jshint.reporter("default"))
-		.pipe(jshint.reporter("fail"))
-		.pipe(insert.prepend('"use strict";\n'))
-		.pipe(uglify())
-		.pipe(insert.transform(function(contents, file) {
-			var str = ['\nworkerScriptUri = (window||this).URL.createObjectURL(new Blob(["'];
-			str.push(contents.replace(/\\/g, "\\\\").replace(/"/g, '\\"'));
-			str.push('"]));');
+        .pipe(jshint({ esversion: 6 }))
+        .pipe(jshint.reporter("default"))
+        .pipe(jshint.reporter("fail"))
+        .pipe(insert.prepend('"use strict";\n'))
+        .pipe(uglify())
+        .pipe(insert.transform(function(contents, file) {
+            var str = ['\nworkerScriptUri = (window||this).URL.createObjectURL(new Blob(["'];
+            str.push(contents.replace(/\\/g, "\\\\").replace(/"/g, '\\"'));
+            str.push('"]));');
+            return str.join("");
+        }));
 
-			return str.join("");
-		}))
-		.pipe(addSrc(["src/ProgressivePromise.js", "src/untar.js"]))
-		.pipe(jshint())
-		.pipe(jshint.reporter("default"))
-		.pipe(jshint.reporter("fail"))
-		.pipe(concat("untar.js"))
-		.pipe(insert.prepend('"use strict";\n'))
-		.pipe(umd({
-			exports: function() { return "untar"; },
-			namespace: function() { return "untar"; }
-		}))
-		.pipe(uglify())
+    // Additional files stream
+    var additionalFiles = gulp.src(["src/ProgressivePromise.js", "src/untar.js"])
+        .pipe(jshint({ esversion: 6 }))
+        .pipe(jshint.reporter("default"))
+        .pipe(jshint.reporter("fail"));
+
+    // Merge all streams, concat, wrap and uglify
+    return merge(workerStream, additionalFiles)
+        .pipe(concat("untar.js"))
+        .pipe(insert.prepend('"use strict";\n'))
+        .pipe(umd({
+            exports: function() { return "untar"; },
+            namespace: function() { return "untar"; }
+        }))
+        .pipe(uglify())
         .pipe(sourcemaps.write("./"))
-		.pipe(gulp.dest("build/dist"));
+        .pipe(gulp.dest("build/dist"));
 });
 
 gulp.task("jshint:specs", function() {
-	return gulp.src("spec/**/*.js")
-		.pipe(jshint())
-		.pipe(jshint.reporter("default"))
-		.pipe(jshint.reporter("fail"));
+    return gulp.src("spec/**/*.js")
+        .pipe(jshint({ esversion: 6 }))
+        .pipe(jshint.reporter("default"))
+        .pipe(jshint.reporter("fail"));
 });
 
-gulp.task("default", ["build:dev", "build:dist"]);
+gulp.task("default", gulp.series("build:dev", "build:dist"));
 
-gulp.task("test", ["jshint:specs", "build:dev", "build:dist"], function(done) {
-	new KarmaServer({
-	    configFile: __dirname + '/karma.conf.js',
-	    singleRun: true
-	  }, done).start();
-});
+gulp.task("test", gulp.series("jshint:specs", "build:dev", "build:dist", function(done) {
+    new KarmaServer({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+}));
 
-gulp.task("example", ["build:dev"], function() {
-	gulp.src("./")
-		.pipe(webserver({
+gulp.task("example", gulp.series("build:dev", function() {
+    gulp.src("./")
+        .pipe(webserver({
             directoryListing: false,
-			livereload: true,
-			open: "example/",
+            livereload: true,
+            open: "example/",
             proxies: [
-                { source: "/base", target: "http://localhost:8000/"}
+                { source: "/base", target: "http://localhost:8000/" }
             ],
             port: 8000
-		}));
-});
+        }));
+}));
